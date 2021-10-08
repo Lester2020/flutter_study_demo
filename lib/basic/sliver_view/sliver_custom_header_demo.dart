0659a8ac47163c94df6d17e7450f50eac7fb778f
Lester2020 <lesterchen2020@outlook.com>
2021-10-08 18:10:58 +0800

    lseter commit

---
+++ lib/basic/sliver_view/sliver_custom_header_demo.dart
@@ -0,0 +1,265 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SliverCustomHeaderDemo extends StatelessWidget {
+  const SliverCustomHeaderDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: SliverCustomHeaderDelegate(
+                44,
+                300,
+                MediaQuery.of(context).padding.top,
+                'https://img.zcool.cn/community/01c6615d3ae047a8012187f447cfef.jpg@1280w_1l_2o_100sh.jpg',
+                '哪吒之魔童降世'
+            ),
+          ),
+          SliverFillRemaining(
+            child: SliverContent(),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final double collapsedHeight;
+  final double expandedHeight;
+  final double paddingTop;
+  final String coverImgUrl;
+  final String title;
+  String statusBarMode = 'dark';
+
+  SliverCustomHeaderDelegate(
+      this.collapsedHeight,
+      this.expandedHeight,
+      this.paddingTop,
+      this.coverImgUrl,
+      this.title);
+
+  @override
+  double get minExtent => this.collapsedHeight + this.paddingTop;
+
+  @override
+  double get maxExtent => this.expandedHeight;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    updateStatusBarBrightness(shrinkOffset);
+    return Container(
+      height: this.maxExtent,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            child: Image.network(
+              this.coverImgUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: this.maxExtent / 2,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0x00000000),
+                    Color(0x90000000)
+                  ]
+                )
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: Container(
+              color: this.headerBgColor(shrinkOffset),
+              child: SafeArea(
+                bottom: false,
+                child: Container(
+                  height: this.collapsedHeight,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: (){
+                          Navigator.of(context).pop();
+                        },
+                        icon: Icon(
+                            Icons.arrow_back_ios,
+                          color: this.headerTextColor(shrinkOffset, true),
+                        ),
+                      ),
+                      Text(
+                        this.title,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: this.headerTextColor(shrinkOffset, false)
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: (){
+                            
+                          }, 
+                          icon: Icon(
+                            Icons.share,
+                            color: this.headerTextColor(shrinkOffset, true),
+                          )
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void updateStatusBarBrightness(double offsetY) {
+    if(offsetY > 50 && this.statusBarMode == "dark") {
+      this.statusBarMode = "light";
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.light
+      ));
+      
+    } else if(offsetY <= 50 && this.statusBarMode == 'light') {
+      this.statusBarMode = 'dark';
+      SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.dark,
+          statusBarIconBrightness: Brightness.dark
+        )
+      );
+    }
+  }
+
+  Color headerBgColor(double offsetY) {
+    int alpha = (offsetY / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
+    return Color.fromARGB(alpha, 255, 255, 255);
+  }
+
+  Color headerTextColor(double offsetY, bool isIcon) {
+    if(offsetY <= 50) {
+      return isIcon ? Colors.white : Colors.transparent;
+    } else {
+      int alpha = (offsetY / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
+      return Color.fromARGB(alpha, 0, 0, 0);
+    }
+  }
+
+}
+
+class SliverContent extends StatelessWidget {
+  const SliverContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Image.network(
+                  'https://img1.gamersky.com/image2019/07/20190725_ll_red_136_2/gamersky_07small_14_201972510258D0.jpg',
+                  width: 130,
+                  height: 180,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(left: 16)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '哪吒之魔童降世',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff333333)
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 10)),
+                  Text(
+                    '动画/中国大陆/110分钟',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xff999999)
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 2)),
+                  Text(
+                    '2019-07-26 08:00 上映',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xff999999),
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 2)),
+                  Text(
+                    '32.1万人想看/大V推荐度95%',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xff999999)
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          Divider(height: 32),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '剧情简介',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff333333)
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(top: 10)),
+              Text(
+                '天地灵气孕育出一颗能量巨大的混元珠，元始天尊将混元珠提炼成灵珠和魔丸，灵珠投胎为人，助周伐纣时可堪大用；而魔丸则会诞出魔王，为祸人间。元始天尊启动了天劫咒语，3年后天雷将会降临，摧毁魔丸。太乙受命将灵珠托生于陈塘关李靖家的儿子哪吒身上。然而阴差阳错，灵珠和魔丸竟然被掉包。本应是灵珠英雄的哪吒却成了混世大魔王。调皮捣蛋顽劣不堪的哪吒却徒有一颗做英雄的心。然而面对众人对魔丸的误解和即将来临的天雷的降临，哪吒是否命中注定会立地成魔？他将何去何从？',
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Color(0xff999999)
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
